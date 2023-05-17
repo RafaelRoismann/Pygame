ee3e31e44646faec2fd29cb7545878fc44d0bf74
Rafael Roismann <rafaelr12@al.insper.edu.brgit config --global user.email>
2023-05-17 17:04:07 -0300

    adicionando arquivo do jogo

---
+++ jogo_da_memoria.py
@@ -0,0 +1,88 @@
+# Jogo da memória
+
+# Criar um dicionário para cada grupo de personagens 
+
+# Definindo o diconário de pokemon 
+dic_pokemon = {}
+dic_pokemon["carta_1"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_2"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_3"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_4"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_5"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_6"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_7"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_8"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_9"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_10"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_11"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_12"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_13"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_14"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_15"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_16"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_17"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+dic_pokemon["carta_18"] = ["foto_frente", "foto_trás", [0, 0], [0,0]] 
+
+#Definindo o dicionário de Star Wars]
+star_wars = {}
+star_wars["carta_1"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_2"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_3"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_4"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_5"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_6"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_7"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_8"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_9"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_10"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_11"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_12"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_13"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_14"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_15"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_16"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_17"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+star_wars["carta_18"] = ["foto_frente", "foto_trás", [0, 0], [0,0]] 
+
+# Definindo o dicionário de Harry Potter
+harry_potter = {}
+harry_potter["carta_1"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_2"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_3"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_4"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_5"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_6"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_7"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_8"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_9"]  = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_10"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_11"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_12"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_13"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_14"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_15"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_16"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_17"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+harry_potter["carta_18"] = ["foto_frente", "foto_trás", [0, 0], [0,0]]
+
+# Escolhendo valores aleatório para cada carta
+
+# Criar uma lista com posições para as 36 cartas
+
+# Atribuir cada uma das posições para cada uma da carta do dicionário escolhido 
+
+# Mostrar cada carta no painel por 5 segundos virada para cima 
+
+# Virar Todas as cartas para baixo 
+
+# Jogador escolhe primeira carta
+
+# Jogador escolhe a segunda 
+
+# Verifica se são iguais 
+
+# Muda o tabuleiro 
+
+# Conta em quantas rodadas foram feitas o jogo 
+
+# Mostra no final um ranking e a sua posição 
